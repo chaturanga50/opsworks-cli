0e9ba203d097e6a908eeb0ac757183cd4f1fe42c
Abeyrathna <UABEYC1@PEROOT.COM>
2018-09-05 18:20:15 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='opsworks-cli',
+    description='A simple python module to work with aws opsworks',
+    url='https://github.com/pypa/sampleproject',
+    version='0.1',
+    scripts=['opsworks-cli']
+)
