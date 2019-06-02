9055fa2d9b53dd0a20fadfc7aec8c723b0e2e518
Chathuranga Abeyrathna <chaturanga50@gmail.com>
2019-06-02 14:25:18 +0400

    update test cases

--- test.py
+++ test.py
@@ -31,42 +31,56 @@ class Case(unittest.TestCase):
             with contextlib.redirect_stdout(usage_stdout):
                 modules.execute_recipes(region=region, stack=stack, cookbook=cookbook)
             output = usage_stdout.getvalue().strip()
-            self.assertIn('execute_recipes main function testing without layer_id and custom_json', output)
+            self.assertIn('execute_recipes main function testing', output)
 
         def test_execute_recipes2(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
                 modules.execute_recipes(region=region, stack=stack, cookbook=cookbook, layer=layer)
             output = usage_stdout.getvalue().strip()
-            self.assertIn('execute_recipes main function testing with layer_id without custom_json', output)
+            self.assertIn('execute_recipes main function testing', output)
 
         def test_execute_recipes3(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
                 modules.execute_recipes(region=region, stack=stack, cookbook=cookbook, custom_json=custom_json)
             output = usage_stdout.getvalue().strip()
-            self.assertIn('execute_recipes main function testing with custom_json and without layer_id', output)
+            self.assertIn('execute_recipes main function testing', output)
 
         def test_execute_recipes4(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
                 modules.execute_recipes(region=region, stack=stack, cookbook=cookbook, layer=layer, custom_json=custom_json)
             output = usage_stdout.getvalue().strip()
-            self.assertIn('execute_recipes main function testing with custom_json and layer_id', output)
+            self.assertIn('execute_recipes main function testing', output)
 
-        def test_run_recipes_without_json(self):
+        def test_run_recipes_without_layer(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
-                modules.run_recipes_without_json(region=region, stack=stack, cookbook=cookbook, layer=layer, instances=instances)
+                modules.run_recipes_without_layer(region=region, stack=stack, cookbook=cookbook, custom_json=custom_json)
             output = usage_stdout.getvalue().strip()
-            self.assertIn('run_recipes_without_json sub function testing without custom_json with layer_id', output)
+            self.assertIn('run_recipes_without_layer sub function', output)
 
-        def test_run_recipes_with_json(self):
+        def test_run_recipes_without_layer2(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
-                modules.run_recipes_with_json(region=region, stack=stack, layer=layer, cookbook=cookbook, instances=instances, custom_json=custom_json)
+                modules.run_recipes_without_layer(region=region, stack=stack, cookbook=cookbook)
             output = usage_stdout.getvalue().strip()
-            self.assertIn('run_recipes_with_json sub function testing with custom_json without layer_id', output)
+            self.assertIn('run_recipes_without_layer sub function', output)
+
+        def test_run_recipes_with_layer(self):
+            usage_stdout = StringIO()
+            with contextlib.redirect_stdout(usage_stdout):
+                modules.run_recipes_with_layer(region=region, stack=stack, layer=layer, cookbook=cookbook, custom_json=custom_json)
+            output = usage_stdout.getvalue().strip()
+            self.assertIn('run_recipes_with_layer sub function', output)
+
+        def test_run_recipes_with_layer2(self):
+            usage_stdout = StringIO()
+            with contextlib.redirect_stdout(usage_stdout):
+                modules.run_recipes_with_layer(region=region, stack=stack, layer=layer, cookbook=cookbook)
+            output = usage_stdout.getvalue().strip()
+            self.assertIn('run_recipes_with_layer sub function', output)
 
         def test_run_update_custom_cookbook(self):
             usage_stdout = StringIO()
