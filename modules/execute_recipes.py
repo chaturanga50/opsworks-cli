8c3c67e1b886cf54f911fe14f651b6bf02f7fbcb
Chathuranga Abeyrathna <chaturanga50@gmail.com>
2019-06-02 14:24:29 +0400

    refactor execute_recipes

--- modules/execute_recipes.py
+++ modules/execute_recipes.py
@@ -16,12 +16,16 @@ def test_output_summary(region, stack, layer, cookbook, custom_json):
     print(table.get_string(title="Test Input Summary"))
 
 
-def run_recipes_with_json(region, stack, layer, cookbook, instances, custom_json=None):
+def run_recipes_with_layer(region, stack, layer, cookbook, custom_json=None):
     # adding new line to support the test functions
     if stack == '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0':
-        print('run_recipes_with_json sub function testing with custom_json without layer_id')
+        print('run_recipes_with_layer sub function')
         test_output_summary(region, stack, layer, cookbook, custom_json)
     else:
+        try:
+            custom_json
+        except NameError:
+            custom_json = str({})
         # initiate boto3 client
         client = boto3.client('opsworks', region_name=region)
         # calling deployment to specified stack
@@ -38,25 +42,37 @@ def run_recipes_with_json(region, stack, layer, cookbook, instances, custom_json
             Comment='automated execute_recipes job with custom_json',
             CustomJson=custom_json
         )
+        # calling aws api to get the instances within the Stack
+        get_intance_count = client.describe_instances(
+            LayerId=layer
+        )
+        all_instance_ids = []
+        for instanceid in get_intance_count['Instances']:
+            ec2id = instanceid['Ec2InstanceId']
+            all_instance_ids.append(ec2id)
+        instances = len(all_instance_ids)
+        # deployment id
         deploymentid = run_recipes['DeploymentId']
         # sending describe command to get status"""  """
         modules.common_functions.get_status(deploymentid, region, instances)
 
 
-def run_recipes_without_json(region, stack, layer, cookbook, instances):
+def run_recipes_without_layer(region, stack, cookbook, custom_json=None):
     # adding new line to support the test functions
     if stack == '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0':
-        print('run_recipes_without_json sub function testing without custom_json with layer_id')
+        print('run_recipes_without_layer sub function')
+        layer = None
         test_output_summary(region, stack, layer, cookbook, custom_json=None)
     else:
+        try:
+            custom_json
+        except NameError:
+            custom_json = str({})
         # initiate boto3 client
         client = boto3.client('opsworks', region_name=region)
-        # calling deployment to specified stack layer
+        # calling deployment to specified stack
         run_recipes = client.create_deployment(
             StackId=stack,
-            LayerIds=[
-                layer,
-            ],
             Command={
                 'Name': 'execute_recipes',
                 'Args': {
@@ -65,94 +81,33 @@ def run_recipes_without_json(region, stack, layer, cookbook, instances):
                     ]
                 }
             },
-            Comment='automated execute_recipes job without custom_json'
+            Comment='automated execute_recipes job without custom_json',
+            CustomJson=custom_json
         )
+        # calling aws api to get the instances within the Stack
+        get_intance_count = client.describe_instances(
+            StackId=stack
+        )
+        all_instance_ids = []
+        for instanceid in get_intance_count['Instances']:
+            ec2id = instanceid['Ec2InstanceId']
+            all_instance_ids.append(ec2id)
+        instances = len(all_instance_ids)
+        # deployment id
         deploymentid = run_recipes['DeploymentId']
         # sending describe command to get status"""  """
         modules.common_functions.get_status(deploymentid, region, instances)
 
 
-def get_instance_count_stack(region, stack, layer, cookbook, custom_json=None):
-    # this function is for where only the stack id is defined
-    # initiate boto3 client
-    client = boto3.client('opsworks', region_name=region)
-    # calling aws api to get the instances within the Stack
-    get_intance_count = client.describe_instances(
-        StackId=stack
-    )
-    all_instance_ids = []
-    for instanceid in get_intance_count['Instances']:
-        ec2id = instanceid['Ec2InstanceId']
-        all_instance_ids.append(ec2id)
-    instances = len(all_instance_ids)
-    # calling function to run
-    if custom_json is None:
-        run_recipes_without_json(region, stack, layer, cookbook, instances)
-    else:
-        run_recipes_with_json(region, stack, layer, cookbook, instances, custom_json)
-
-
-def get_instance_count_layer(region, stack, layer, cookbook, custom_json=None):
-    # this function is for where the stack and layer both ids are defined
-    # initiate boto3 client
-    client = boto3.client('opsworks', region_name=region)
-    # calling aws api to get the instances within the layer
-    get_intance_count = client.describe_instances(
-        LayerId=layer
-    )
-    all_instance_ids = []
-    for instanceid in get_intance_count['Instances']:
-        ec2id = instanceid['Ec2InstanceId']
-        all_instance_ids.append(ec2id)
-    instances = len(all_instance_ids)
-    # calling function to run
-    if custom_json is None:
-        run_recipes_without_json(region, stack, layer, cookbook, instances)
-    else:
-        run_recipes_with_json(region, stack, layer, cookbook, instances, custom_json)
-
-
 def execute_recipes(region, stack, cookbook, layer=None, custom_json=None):
-    if custom_json is None and layer is None:
-        # adding new line to support the test functions
-        if stack == '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0':
-            print('execute_recipes main function testing without layer_id and custom_json')
-            test_output_summary(region, stack, layer, cookbook, custom_json)
-        else:
-            # sending request to collect the stack and layer names
-            modules.common_functions.get_names(stack, layer, region, "execute_recipe")
-            print("\ncookbook " + str(cookbook) + " | and without layer or custom-json")
-            # sending request to get the instance count with stack and send to boto3
-            get_instance_count_stack(region, stack, layer, cookbook, custom_json)
-    elif custom_json is None and layer is not None:
-        # adding new line to support the test functions
-        if stack == '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0':
-            print('execute_recipes main function testing with layer_id without custom_json')
-            test_output_summary(region, stack, layer, cookbook, custom_json)
-        else:
-            # sending request to collect the stack and layer names
-            modules.common_functions.get_names(stack, layer, region, "execute_recipe")
-            print("\ncookbook " + str(cookbook) + " | and with layer_id and without custom-json")
-            # sending request to get the instance count with stack and send to boto3
-            get_instance_count_layer(region, stack, layer, cookbook)
-    elif layer is None and custom_json is not str({}):
-        # adding new line to support the test functions
-        if stack == '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0':
-            print('execute_recipes main function testing with custom_json and without layer_id')
-            test_output_summary(region, stack, layer, cookbook, custom_json)
-        else:
-            # sending request to collect the stack and layer names
-            modules.common_functions.get_names(stack, layer, region, "execute_recipe")
-            print("\ncookbook " + str(cookbook) + " | and custom-json " + str(custom_json))
-            # sending request to get the instance count with stack and send to boto3
-            get_instance_count_stack(region, stack, layer, cookbook, custom_json)
+    # adding new line to support the test functions
+    if stack == '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0':
+        print('execute_recipes main function testing')
+        test_output_summary(region, stack, layer, cookbook, custom_json)
     else:
-        # adding new line to support the test functions
-        if stack == '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0':
-            print('execute_recipes main function testing with custom_json and layer_id')
-            test_output_summary(region, stack, layer, cookbook, custom_json)
+        # sending request to collect the stack and layer names
+        modules.common_functions.get_names(stack, layer, region, "execute_recipe")
+        if layer is None:
+            run_recipes_without_layer(region, stack, cookbook, custom_json)
         else:
-            modules.common_functions.get_names(stack, layer, region, "execute_recipe")
-            print("\ncookbook " + str(cookbook) + " | custom-json " + str(custom_json) + " | layer " + str(layer))
-            # sending request to get the instance count with stack and send to boto3
-            get_instance_count_layer(region, stack, layer, cookbook)
+            run_recipes_with_layer(region, stack, layer, cookbook, custom_json)
