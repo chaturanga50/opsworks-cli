bcfdd97170e6685afa2531a9e082f501cdbfa351
Chathuranga Abeyrathna <chaturanga50@gmail.com>
2019-06-14 12:53:49 +0400

    updated test file and others

--- test.py
+++ test.py
@@ -13,9 +13,10 @@ import modules.deploy
 import modules.setup
 import modules.common_functions
 
-region = 'eu-west-1'
-stack = '2e7f6dd5-e4a3-4389-bc95-b4bacc234df0'
-layer = 'ac0df176-104b-46ae-946e-7cf7367b816e'
+region = 'us-west-2'
+stack = '6f210730-19ec-4369-b1dc-0148480d5a56'
+layer = '0d5e11ed-36d4-42d0-a353-fbf7d6c80410'
+app = '2da891ea-1809-480d-a799-cb2c08746115'
 cookbook = 'apache2::default'
 custom_json = '{"default": "version"}'
 instances = 2
@@ -148,28 +149,28 @@ class Case(unittest.TestCase):
         def test_run_deploy_with_layer(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
-                modules.deploy_with_layer(region=region, stack=stack, layer=layer, custom_json=custom_json)
+                modules.deploy_with_layer(region=region, stack=stack, layer=layer, app=app, custom_json=custom_json)
             output = usage_stdout.getvalue().strip()
             self.assertIn('deploy_with_layer sub function', output)
 
         def test_run_deploy_with_layer2(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
-                modules.deploy_with_layer(region=region, stack=stack, layer=layer)
+                modules.deploy_with_layer(region=region, stack=stack, layer=layer, app=app)
             output = usage_stdout.getvalue().strip()
             self.assertIn('deploy_with_layer sub function', output)
 
         def test_run_deploy_without_layer(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
-                modules.deploy_without_layer(region=region, stack=stack, custom_json=custom_json)
+                modules.deploy_without_layer(region=region, stack=stack, app=app, custom_json=custom_json)
             output = usage_stdout.getvalue().strip()
             self.assertIn('deploy_without_layer sub function', output)
 
         def test_run_deploy_without_layer2(self):
             usage_stdout = StringIO()
             with contextlib.redirect_stdout(usage_stdout):
-                modules.deploy_without_layer(region=region, stack=stack)
+                modules.deploy_without_layer(region=region, stack=stack, app=app)
             output = usage_stdout.getvalue().strip()
             self.assertIn('deploy_without_layer sub function', output)
 
