010fa3415f93675bf912a582ecd2fbf97db88d22
Chathuranga Abeyrathna <chaturanga50@gmail.com>
2019-07-19 14:22:05 +0400

    tag version 0.5.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
-    version="0.5.1",
+    version="0.5.2",
     install_requires=[
         'boto3',
         'prettytable'
